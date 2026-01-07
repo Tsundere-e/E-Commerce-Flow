14ae4ddd6d1e08e7261aff31ec8e04dc88808b5b
Ana Luisa <analuisa.r.dos.s@gmail.com>
2026-01-07 15:00:01 -0300

    Forçando import do tailwind pelo JS 🎀

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import 'tailwindcss/index.css' // Importa direto do pacote para evitar erro de arquivo local
 import './index.css'
 import App from './App.jsx'
 
