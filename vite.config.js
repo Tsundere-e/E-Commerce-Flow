2680e32dea8d3419e3dc04ec8f05d6204f78b9d3
Ana Luisa <analuisa.r.dos.s@gmail.com>
2026-01-07 14:47:40 -0300

    Final link between Vite and CSS engine 🎀

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite' // Esta linha permite ler o seu index.css
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(), // Esta linha processa o @import "tailwindcss"
+  ],
 })
